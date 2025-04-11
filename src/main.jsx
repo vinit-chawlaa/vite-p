f805edcc310ced6c04fbe36c0812517ccc63f55c
Vinit Chawla <vinit223@gmail.com>
2025-04-11 22:00:28 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Color from './Color.jsx'
-import Tip from './Tip.jsx'
-import Subscribe from './Subscribe.jsx'
-import File from './File.jsx'
-import Dashboard from './Dashboard.jsx'
-import Counter from './Counter.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Input from './Input.jsx'
-import Date from './Date.jsx'
-import Emoji from './Emoji.jsx'
-import Otp from './Otp.jsx'
 import Main from './components/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
     <Main />
-    </BrowserRouter>
   </StrictMode>,
   
 )
