8fbff5d89f6d7fd86e44439f4b1a39d1ac0d2bbc
Vinit Chawla <vinit223@gmail.com>
2025-04-11 13:16:47 +0530

    new

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,12 +29,15 @@ const Project = () => {
                 <motion.div initial={{x : 100 , opacity : 0}} viewport={{ once: true }} whileInView={{x : 0 , opacity : 1}} transition={{duration : 0.5 , delay : 0.4}} className='w-full  min-h-[300px]  hover:shadow-[0_20px_50px_-10px_rgba(109,40,217,0.4)] rounded-3xl border-t-5 border-t-[#4A2084] p-3 bg-gradient-to-tl from-[#2e0a51] via-[#0d0219] to-[rgb(34,6,64)]'>
                     <div className='grid sm:grid-cols-[40%_auto] grid-cols-1 lg:gap-10 gap-5 justify-items-center items-center'>
                         <div>
-                            <img src="/deepseek.png" alt="" className='object-contain w-full h-full' />
+                            <img src="/yoga.png" alt="" className='object-contain w-full h-full' />
                         </div>
+                    
                         <div>
-                            <h2 className='text-3xl py-5'>DeepSeek</h2>
-                            <p className='text-md text-gray-300'>DeepSeek Ai built with Next.js and Tailwind CSS and Shadcin Ui.</p>
+                            <h2 className='text-3xl py-5'>FoodYoga</h2>
+                            <p className='text-md text-gray-300'>FoodYoga (Official windows server website) built with Next.js and Tailwind CSS.</p>
+                            <a href="https://foodyoga.vercel.app/">
                             <button className='px-5 py-2 my-5 cursor-pointer  hover:from-[#5B21B6] hover:to-[#6D28D9] transition-all duration-300 text-white rounded-md bg-gradient-to-r from-[#6D28D9] to-[#5B21B6]'>Live Demo</button>
+                            </a>
                         </div>
                     </div>
                 </motion.div>
@@ -50,7 +53,9 @@ const Project = () => {
                         <div>
                             <h2 className='text-3xl py-5'>Weather App</h2>
                             <p className='text-md text-gray-300'>Weather App built with react.js and Tailwind CSS and Shadcin Ui.</p>
-                            <button className='px-5 py-2 my-5 cursor-pointer  hover:from-[#5B21B6] hover:to-[#6D28D9] transition-all duration-300 text-white rounded-md bg-gradient-to-r from-[#6D28D9] to-[#5B21B6]'>Live Demo</button>
+                           <a href="https://vinit-chawlaa.github.io/Weathersapp2/">
+                           <button className='px-5 py-2 my-5 cursor-pointer  hover:from-[#5B21B6] hover:to-[#6D28D9] transition-all duration-300 text-white rounded-md bg-gradient-to-r from-[#6D28D9] to-[#5B21B6]'>Live Demo</button>
+                           </a>
                         </div>
                     </div>
                 </motion.div>
@@ -58,14 +63,16 @@ const Project = () => {
 
                 <Tilt tiltMaxAngleX={15} tiltMaxAngleY={15} transitionSpeed={1500} glareEnable={false}>
                 <motion.div initial={{x : 100 , opacity : 0}} viewport={{ once: true }} whileInView={{x : 0 , opacity : 1}} transition={{duration : 0.5 , delay : 0.8}} className='w-full min-h-[300px]  hover:shadow-[0_20px_50px_-10px_rgba(109,40,217,0.4)] rounded-3xl border-t-5 border-t-[#4A2084] p-3 bg-gradient-to-tl from-[#2e0a51] via-[#0d0219] to-[rgb(34,6,64)]'>
-                    <div className='grid sm:grid-cols-[40%_auto] grid-cols-1 lg:gap-10 gap-5 justify-items-center items-center'>
+                    <div className='grid sm:grid-cols-[40%_auto] grid-cols-1 lg:gap-10  justify-items-center items-center'>
                         <div>
                             <img src="/man.png" alt="" className='object-contain h-full w-full' />
                         </div>
                         <div>
                             <h2 className='text-3xl py-5'>Portfolio website</h2>
                             <p className='text-md text-gray-300'>Portfolio website built with Next.js and Tailwind CSS and Shadcin Ui.</p>
+                            <a href="/">
                             <button className='px-5 py-2 my-5 cursor-pointer  hover:from-[#5B21B6] hover:to-[#6D28D9] transition-all duration-300 text-white rounded-md bg-gradient-to-r from-[#6D28D9] to-[#5B21B6]'>Live Demo</button>
+                            </a>
                         </div>
                     </div>
                 </motion.div>
